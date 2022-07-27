a0d116dd381e8b3f0f94f2c2827eb511169c11e1
yasar2385 <yasar.mohideen@newgen.co>
2022-07-27 22:41:41 +0530

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const input = document.getElementById('input');
+const position = input.selectionStart;
+
+// If the position of the cursor is at the very last character inside the input,
+// this will result in 3
+//alert(position);
+
+document.getElementById("trigger").addEventListener("click", function(){
+  var myElement = document.getElementById('text-element');
+  var startPosition = myElement.selectionStart;
+  var endPosition = myElement.selectionEnd;
+  
+  // Check if you've selected text
+  if(startPosition == endPosition){
+      alert("The position of the cursor is (" + startPosition + "/" + myElement.value.length + ")");
+  }else{
+      alert("Selected text from ("+ startPosition +" to "+ endPosition + " of " + myElement.value.length + ")");
+  }
+},false);
+
+document.getElementById('input').addEventListener('keyup', e => {
+  console.log('Caret at: ', e.target.selectionStart)
+})
