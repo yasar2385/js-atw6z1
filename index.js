2bca78708b07f1e5593eb007ed8e51fb6ae13545
yasar2385 <yasar.mohideen@newgen.co>
2022-07-27 23:42:30 +0530

    1 file modified

--- index.js
+++ index.js
@@ -45,14 +45,41 @@ const getCode = (e) => {
   e = e || window.event;
   return e.code;
 };
-var BK_SB_DEL = ['Delete','Space','Backspace']
+var BK_SB_DEL = ['Space', 'Delete', 'Backspace'];
+var GET_TEXT_VALUE = function (idx) {
+  let txt = document.getElementById('input').value;
+  //console.log(txt, idx);
+  return txt.charAt(idx);
+};
 document.getElementById('input').addEventListener('keydown', (e) => {
   //console.log('Caret at: ', e.target.selectionStart);
+  var start = e.target.selectionStart;
+  var end = e.target.selectionEnd;
+  //console.log(start, end);
   let key = getCode(e);
-  console.log(key);
-  if (isFinite(key)) {
-    console.log(`Number ${key} was pressed!`);
+  let IsCancelable = !1;
+  //console.log(e.code);
+  let del_key_indx = BK_SB_DEL.includes(e.code);
+  let start_text = GET_TEXT_VALUE(start);
+  //console.log('start_text ==> ' + start_text);
+  //console.log(del_key_indx);
+  console.log(del_key_indx && del_key_indx > -1);
+  if (del_key_indx && del_key_indx > -1) {
+      console.log(`Remove ${key} was pressed!`);
+    if ((del_key_indx = 0)) IsCancelable = !0;
+    else {
+      start_text = GET_TEXT_VALUE(del_key_indx == 1 ? start : start - 1);
+      if (!isNaN(start_text)) IsCancelable = !0;
+    }
+  } else if (!isNaN(e.key)) {
+    IsCancelable = !0;
+    //console.log(`Number ${key} was pressed!`);
   } else {
     //console.log(`alphabetic ${key} was pressed!`);
+  }
+
+  if (IsCancelable) {
+    e.preventDefault();
+    return false;
   }
 });
