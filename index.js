0e29e6e61152a85801bf845e5bf648d61308db5a
yasar2385 <yasar.mohideen@newgen.co>
2022-07-27 22:53:04 +0530

    1 file modified

--- index.js
+++ index.js
@@ -11,19 +11,47 @@ const position = input.selectionStart;
 // this will result in 3
 //alert(position);
 
-document.getElementById("trigger").addEventListener("click", function(){
-  var myElement = document.getElementById('text-element');
-  var startPosition = myElement.selectionStart;
-  var endPosition = myElement.selectionEnd;
-  
-  // Check if you've selected text
-  if(startPosition == endPosition){
-      alert("The position of the cursor is (" + startPosition + "/" + myElement.value.length + ")");
-  }else{
-      alert("Selected text from ("+ startPosition +" to "+ endPosition + " of " + myElement.value.length + ")");
-  }
-},false);
+document.getElementById('trigger').addEventListener(
+  'click',
+  function () {
+    var myElement = document.getElementById('text-element');
+    var startPosition = myElement.selectionStart;
+    var endPosition = myElement.selectionEnd;
 
-document.getElementById('input').addEventListener('keyup', e => {
-  console.log('Caret at: ', e.target.selectionStart)
-})
+    // Check if you've selected text
+    if (startPosition == endPosition) {
+      alert(
+        'The position of the cursor is (' +
+          startPosition +
+          '/' +
+          myElement.value.length +
+          ')'
+      );
+    } else {
+      alert(
+        'Selected text from (' +
+          startPosition +
+          ' to ' +
+          endPosition +
+          ' of ' +
+          myElement.value.length +
+          ')'
+      );
+    }
+  },
+  false
+);
+const getCode = (e) => {
+  e = e || window.event;
+  return e.code;
+};
+document.getElementById('input').addEventListener('keydown', (e) => {
+  //console.log('Caret at: ', e.target.selectionStart);
+  let key = getCode(e);
+  console.log(key);
+  if (isFinite(key)) {
+    console.log(`Number ${key} was pressed!`);
+  } else {
+    //console.log(`alphabetic ${key} was pressed!`);
+  }
+});
